7a939a5f767dc8487010682ccf5fff8fa1b43db0
Lumieregyn <lumieregyn.ai@gmail.com>
2025-04-24 05:14:36 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
+app.use("/conversa", require("./routes/analise"));
+
 app.get("/", (req, res) => {
-  res.json({
-    status: "ok",
-    mensagem: "Servidor webhook ativo para análise de conversas."
-  });
+    res.json({ status: "ok", mensagem: "Servidor webhook ativo para análise de conversas." });
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
